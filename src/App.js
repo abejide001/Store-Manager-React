9495a011c30fb6d9a86200b72031c20b7255e711
abejide001 <abejidefemi1@gmail.com>
2019-01-12 22:32:54 +0100

    chore(setup): Setup development environment Install babel, webpack and eslint Set up local server [Finishes #163129022]

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello Welcome
+      </div>
+    );
+  }
+}
