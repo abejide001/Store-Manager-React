87f18218868e50f8add8187db5264ae70ee12b0c
abejide001 <abejidefemi1@gmail.com>
2019-03-05 17:30:06 +0100

    feat(search): Implement search products Write test for the implementation [Delivers 164398723]

---
+++ __tests__/__mocks__/attendantMock.js
@@ -0,0 +1,11 @@
+const props = {
+  getProducts: () => null,
+  product: {
+    products: {
+      products: {
+        value: ['ool', 'ool'].filter(jest.fn()),
+      },
+    },
+  },
+};
+export default props;
